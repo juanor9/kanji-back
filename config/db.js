426ad3b02f6cdaabc3cc67e5468cf2b1f216ca00
Juan Camilo Orjuela <orjuela9@gmail.com>
2024-05-06 07:36:16 -0500

    set db config

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', false);
+async function connectDb() {
+  const uri = process.env.MONGO_DB_URI;
+
+  if (!uri) {
+    throw new Error('MONGO_DB_URI is not defined');
+  }
+
+  try {
+    await mongoose.connect(uri);
+
+    console.log('🔗 Conectado a MongoDB');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+export default connectDb;
