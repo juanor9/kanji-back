be60d4006233c6d2d729f3591ecd783c44e1ae7f
Juan Camilo Orjuela <orjuela9@gmail.com>
2024-05-24 10:22:39 -0500

    include query to get a random kanji

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,15 +2,20 @@ import Character from "../models/Character.js";
 
 const resolvers = {
   Query: {
+    // Resolver para obtener un kanji por su ID
     getCharacterById: async (_, { id }) => {
       try {
         const character = await Character.findById(id);
+        if (!character) {
+          throw new Error('Character not found');
+        }
         return character;
       } catch (error) {
         console.error("Error en getCharacterById:", error);
         throw new Error('Error fetching character by ID');
       }
     },
+    // Resolver para obtener todos los kanji
     getAllCharacters: async () => {
       try {
         const characters = await Character.find();
@@ -19,8 +24,23 @@ const resolvers = {
         console.error("Error en getAllCharacters:", error);
         throw new Error('Error fetching all characters');
       }
+    },
+    getRandomCharacter: async () => {
+      try {
+        const count = await Character.countDocuments();
+        if (!count || count <= 0){
+          throw new Error('Error getting all characters');
+        }
+        const random = Math.floor(Math.random() * count);
+        const character = await Character.findOne().skip(random);
+        return character;
+      } catch (error) {
+        console.error("Error en getRandomCharacter:", error);
+        throw new Error('Error fetching random character');
+      }
     }
   }
 };
 
 export default resolvers;
+
